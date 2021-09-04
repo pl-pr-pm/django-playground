714491312a5fc831550e05e434c8810323307c2f
pl-pr-pm <pl.pr.pm.12@gmail.com>
2021-09-04 11:01:33 +0900

    chapter1_2

---
+++ snippets/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+
+def top(request):
+    return HttpResponse(b"Hello World")
+
+
+def snippet_new(request):
+    return HttpResponse('スニッペットの登録')
+
+
+def snippet_edit(request, snippet_id):
+    return HttpResponse('スニッペットの編集')
+
+
+def snippet_detail(request, snippet_id):
+    return HttpResponse('スニッペットの詳細閲覧')
