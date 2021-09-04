00dc10f405acf283ed98e5ed809ac5e6dfe688e4
pl-pr-pm <pl.pr.pm.12@gmail.com>
2021-09-04 11:15:45 +0900

    1.2終了

--- snippets/tests.py
+++ snippets/tests.py
@@ -1,33 +1,8 @@
 from django.test import TestCase
 
 # Create your tests here.
-from django.http import HttpRequest
+from django.urls import resolve
 from snippets.views import top, snippet_new, snippet_edit, snippet_detail
-
-
-"""
-class TopPageViewTest(TestCase):
-    def test_top_returns_200(self):
-        request = HttpRequest()
-        response = top(request)
-        self.assertEqual(response.status_code, 200)
-    
-    
-    def test_top_returns_expected_content(self):
-        request = HttpRequest()
-        response = top(request)
-        self.assertEqual(response.content, b"Hello World")
-
-class TopPageTest(TestCase):
-    def test_top_returns_200(self):
-        response = self.client.get("/")
-        self.assertEqual(response.status_code, 200)
-    
-    
-    def test_top_returns_expected_content(self):
-        response = self.client.get("/")
-        self.assertEqual(response.content, b"Hello World")
-"""
 
 class CreateSnippetTest(TestCase):
     def test_should_resolve_snippet_new(self):
