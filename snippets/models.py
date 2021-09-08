9e34109999a613bdb0cae24988f04f63a253c89b
pl-pr-pm <pl.pr.pm.12@gmail.com>
2021-09-08 21:56:45 +0900

    1.6.4

--- snippets/models.py
+++ snippets/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+from django.conf import settings
+
+
+class Snippet(models.Model):
+    title = models.CharField('タイトル', max_length=128)
+    code = models.TextField('コード', blank=True)
+    description = models.TextField('説明', blank=True)
+    #created_by = models.ForeignKey('settings.AUTH_USER_MODEL',
+    #                               verbose_name="投稿者",
+    #                               on_delete=models.CASCADE) # ユーザが削除された際に、そのユーザへの参照をもつスニペットレコードも自動的に削除される。障害発生時にユーザが削除されると自動でスニペットモデルも削除されるのは痛いなぁ。不整合が発生しなくなるけど。。。
+    created_at = models.DateTimeField('投稿日', auto_now_add=True)
+    updated_at = models.DateTimeField('更新日', auto_now_add=True)
+
+    def __str__(self):
+        return self.title
--- snippets/views.py
+++ snippets/views.py
@@ -26,7 +26,7 @@ def snippet_edit(request, snippet_id):
     snippet = get_object_or_404(Snippet, pk=snippet_id)
     #if snippet.created_by_id != request.user.id:
     #    return HttpResponseForbidden("このスニペットの編集は許可されていません。")
-    
+
     if request.method == "POST":
         form = SnippetForm(request.POST, instance=snippet)
         if form.is_valid():
